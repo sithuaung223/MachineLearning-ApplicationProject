9a78e6acce9f7aaa318dcb9cffeb496f687a8243
sithuaung223 <sithuaung223@gmail.com>
2018-03-01 00:12:20 -0600

    applied 10 fold cross validation to linearRegression Model

--- milestone_1/plot_bike.py
+++ milestone_1/plot_bike.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import KFold
 
 # Load the bike dataset
 bike_X = []
@@ -24,19 +25,10 @@ bike_X = np.array(bike_X)
 bike_Y = np.array(bike_Y)
 # 80% train data and 20% test data 
 test_dataSize = int(len(bike_X) * 0.2)
-#print("test_dataSize", test_dataSize)
-#print("X", bike_X[0])
-#print("Y", bike_Y[0])
-#sys.exit(0)
 
 # Split the data into training/testing sets
 bike_X_train = bike_X[:-test_dataSize]
 bike_X_test = bike_X[-test_dataSize:]
-#np.reshape(bike_X_train, -1, bike_X_train.shape[0])
-#np.reshape(bike_X_test, -1, bike_X_test.shape[0]) 
-print("X_train", bike_X_train.shape)
-print("X_test", bike_X_test.shape)
-#sys.exit(0)
 
 
 # Split the targets into training/testing sets
@@ -46,16 +38,29 @@ bike_Y_test = bike_Y[-test_dataSize:]
 # Create linear regression object
 regr = linear_model.LinearRegression()
 
-# Train the model using the training sets
-regr.fit(bike_X_train, bike_Y_train)
+sum_MSE = 0
+kf = KFold(n_splits = 10, shuffle = True, random_state=0)
+for train, test in kf.split(bike_X_train):
+    # Split the data into training/testing sets
+    X_CVtrain, X_CVtest = bike_X_train[train] , bike_X_train[test]
+
+    # Split the targets into training/testing sets
+    Y_CVtrain, Y_CVtest = bike_Y_train[train] , bike_Y_train[test]
+
+    # Train the model using the training sets
+    regr.fit(X_CVtrain, Y_CVtrain)
+
+    # Make predictions using the testing set
+    Y_CVpred = regr.predict(X_CVtest)
+    
+    sum_MSE += mean_squared_error(Y_CVtest, Y_CVpred)
+
+avg = sum_MSE/10.00
+print("Avearge Mean squared error: %2.f" % avg)
 
 # Make predictions using the testing set
 bike_Y_pred = regr.predict(bike_X_test)
 
-print("X_train", bike_X_train.shape)
-print("X_test", bike_X_test.shape)
-print("Y_train", bike_Y_train.shape)
-print("Y_test", bike_Y_test.shape)
 # The coefficients
 print('Coefficients: \n', regr.coef_)
 # The mean squared error
